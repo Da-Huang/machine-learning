e95cfbe087d4a934153b3f8c84b6a0caad94f44c
Da Huang <dhuang.cn@gmail.com>
2016-06-18 08:07:29 +0800

    ex4 q19
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- ntuml1/ex4/q19.m
+++ ntuml1/ex4/q19.m
@@ -1,33 +1,37 @@
-function [log_lambda err_in err_out] = q16()
+function [log_lambda err_cv err_in err_out] = q19(fold=5)
   log_lambda = 2:-1:-10;
   lambda = 10 .^ log_lambda;
 
   data = load('train13.dat');
-  train = data(1:120, :);
-  val = data(121:end, :);
   test = load('test13.dat');
 
-  trainx = train(:, 1:end-1);
-  trainy = train(:, end);
-  valx = val(:, 1:end-1);
-  valy = val(:, end);
+  N = size(data, 1);
+  Ni = N / fold;
+
   testx = test(:, 1:end-1);
   testy = test(:, end);
 
   ans.lambda = [];
-  ans.err_in = [];
-  ans.err_val = [];
-  ans.err_out = [];
+  ans.err_cv = [];
 
   for l = lambda
     ans.lambda(end+1) = l;
-    w = reg(trainx, trainy, l);
-    ans.err_in(end+1) = get_err(trainx, w, trainy);
-    ans.err_val(end+1) = get_err(valx, w, valy);
-    ans.err_out(end+1) = get_err(testx, w, testy);
+    err = 0;
+    for k = 1:Ni:N
+      train = [data(1:k-1, :); data(k+Ni:end, :)];
+      val = data(k:k+Ni-1, :);
+      trainx = train(:, 1:end-1);
+      trainy = train(:, end);
+      valx = val(:, 1:end-1);
+      valy = val(:, end);
+
+      w = reg(trainx, trainy, l);
+      err += get_err(valx, w, valy);
+    end
+    ans.err_cv(end+1) = err / fold;
   end
 
-  [err_val i] = min(ans.err_val);
+  [err_cv i] = min(ans.err_cv);
   lambda = ans.lambda(i);
   log_lambda = log10(lambda);
   trainx = data(:, 1:end-1);
