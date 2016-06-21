1e5f5e2abe7bf6e5bff71c3ceb08e3f16a5a8556
Da Huang <dhuang.cn@gmail.com>
2016-06-21 10:03:56 +0800

    ex1 q16
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

---
+++ ntuml2/ex1/q16.m
@@ -0,0 +1,9 @@
+function q16()
+  addpath('../../libsvm/matlab');
+  train = load('train16.dat');
+  X = train(:,2:end);
+  y = double(train(:,1) == 0);
+  model = svmtrain(y, X, '-s 0 -t 0 -c 0.01 -h 0 -q');
+  w = model.SVs' * model.sv_coef;
+  norm(w)
+endfunction
